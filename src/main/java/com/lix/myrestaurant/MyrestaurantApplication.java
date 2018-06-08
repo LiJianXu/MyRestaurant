cf1b3c94efedf62d8bcb002f9fde523144b84adb
ljx <1582722349@qq.com>
2018-06-08 11:28:39 +0800

    初始化项目

---
+++ src/main/java/com/lix/myrestaurant/MyrestaurantApplication.java
@@ -0,0 +1,11 @@
+package com.lix.myrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrestaurantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyrestaurantApplication.class, args);
+    }
+}
